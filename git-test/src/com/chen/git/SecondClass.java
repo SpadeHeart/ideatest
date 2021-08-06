5740c3da782aad105c2815acbd903981c677f372
chen <486701367@qq.com>
2021-08-06 22:15:06 +0800

    第一次提交

---
+++ git-test/src/com/chen/git/SecondClass.java
@@ -0,0 +1,9 @@
+package com.chen.git;
+
+import java.util.Arrays;
+
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("args = " + Arrays.deepToString(args));
+    }
+}
